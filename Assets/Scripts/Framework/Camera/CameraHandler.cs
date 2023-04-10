d6aef50eb579ca703f5ed72b23b306b632e71974
Ashechol <ashechol@qq.com>
2023-04-10 19:49:52 +0800

    Working on character and camera

--- Assets/Scripts/Framework/Camera/CameraHandler.cs
+++ Assets/Scripts/Framework/Camera/CameraHandler.cs
@@ -30,12 +30,14 @@ namespace Framework.Camera
 
         private void Start()
         {
-            _yaw = cameraRoot.rotation.eulerAngles.y;
+            _yaw = cameraRoot.localRotation.eulerAngles.y;
         }
 
         private void Update()
         {
+            #if UNITY_EDITOR
             if (!Application.isPlaying) return;
+            #endif
             
             CameraRotation();
         }
@@ -59,6 +61,7 @@ namespace Framework.Camera
         {
             _yaw += _input.YawInput * Time.deltaTime * 50;
             _yaw = Functions.ClampAngle(_yaw, 0, 360);
+            // 必须是 global rotation 才能解绑子物体的旋转和父物体
             cameraRoot.rotation = Quaternion.Euler(0, _yaw, 0);
         }
     }
